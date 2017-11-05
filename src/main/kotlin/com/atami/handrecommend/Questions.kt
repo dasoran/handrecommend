f81780d058e7cfa3e0d8eb1e474fd02254d8d750
dasoran <dasoran@gmail.com>
2017-11-05 09:26:19 +0900

    add mock data(in code)

--- src/main/kotlin/com/atami/handrecommend/Questions.kt
+++ src/main/kotlin/com/atami/handrecommend/Questions.kt
@@ -4,10 +4,39 @@ import io.ktor.application.*
 import io.ktor.locations.*
 import io.ktor.response.*
 import io.ktor.routing.*
+import io.ktor.http.*
 
 
 fun Route.index() {
     get<Questions> {
-        call.respondText("Hello, World!")
+        call.respondText("[\n" +
+                "  {\n" +
+                "    \"question_id\": 1,\n" +
+                "    \"user_id\": 1,\n" +
+                "    \"title\": \"近代・現代日本史の本を教えてください\",\n" +
+                "    \"recommend_count\": 5,\n" +
+                "    \"bet_cash\": 100,\n" +
+                "    \"cash_unit\": \"jpy\",\n" +
+                "    \"is_close\": false\n" +
+                "  },\n" +
+                "  {\n" +
+                "    \"question_id\": 2,\n" +
+                "    \"user_id\": 1,\n" +
+                "    \"title\": \"中国の三国志の本を教えてください\",\n" +
+                "    \"recommend_count\": 5,\n" +
+                "    \"bet_cash\": 100,\n" +
+                "    \"cash_unit\": \"jpy\",\n" +
+                "    \"is_close\": false\n" +
+                "  },\n" +
+                "  {\n" +
+                "    \"question_id\": 3,\n" +
+                "    \"user_id\": 2,\n" +
+                "    \"title\": \"おすすめの漫画を教えてください\",\n" +
+                "    \"recommend_count\": 5,\n" +
+                "    \"bet_cash\": 100,\n" +
+                "    \"cash_unit\": \"jpy\",\n" +
+                "    \"is_close\": false\n" +
+                "  }\n" +
+                "]", ContentType.Application.Json)
     }
 }
